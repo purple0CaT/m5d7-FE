09c830acbf61d620a93bb2d28cda84107de00203
Istvan <steven.ford.j@gmail.com>
2021-09-21 17:57:49 +0300

    design fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,25 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="light" variant="light">
       <Navbar.Brand as={Link} to="/">
         Amazon
       </Navbar.Brand>
       <Nav className="mr-auto">
-        <Link to="/" style={{ marginRight: "2em", textDecoration: "none" }}>
+        <Link to="/" className='text-dark' style={{ marginRight: "2em", textDecoration: "none" }}>
           Home
         </Link>
-        <Link to="/products" style={{ marginRight: "2em" }}>
+        <Link to="/products" className='text-dark' style={{ marginRight: "2em" }}>
           Products
         </Link>
-        <Link to="/newproduct" style={{ marginRight: "2em" }}>
+        <Link to="/newproduct" className='text-dark' style={{ marginRight: "2em" }}>
           Add a product
         </Link>
       </Nav>
-      <Form inline>
+      {/* <Form inline>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
         <Button variant="outline-info">Search</Button>
-      </Form>
+      </Form> */}
     </Navbar>
   );
 };
